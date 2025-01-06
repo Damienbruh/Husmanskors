46294062e9ea3dde63550b95ff9b511335ebe1d9
David Hansén <31345425+davezargar@users.noreply.github.com.>
2025-01-06 11:56:46 +0100

    smal

--- app/Program.cs
+++ app/Program.cs
@@ -6,7 +6,7 @@ class Program
 {
     static async Task Main()
     {
-        Env.TraversePath().Load();  //laddar in variabler från .env filen behöver en variabel exempel nedaför när vi har skapat vår db
+        Env.TraversePath().Load();//laddar in variabler från .env filen behöver en variabel exempel nedaför när vi har skapat vår db
         // DBConnectString="Host=localhost;Port=5432;Username=postgres;Password=pass123;Database=dbname;SearchPath=public"
 
         DatabaseConnection database = new();

c5602af1598a335b0459dbaed5388871f8c32e66
David Hansén <31345425+davezargar@users.noreply.github.com.>
2025-01-06 10:05:10 +0100

    basic project setup

--- app/Program.cs
+++ app/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DotNetEnv;
+using app.Database;
+namespace app;
+
+class Program
+{
+    static async Task Main()
+    {
+        Env.TraversePath().Load();  //laddar in variabler från .env filen behöver en variabel exempel nedaför när vi har skapat vår db
+        // DBConnectString="Host=localhost;Port=5432;Username=postgres;Password=pass123;Database=dbname;SearchPath=public"
+
+        DatabaseConnection database = new();
+        
+
+        Console.WriteLine("Program will now exit, press any key to continue");
+        Console.ReadKey();
+    }
+}

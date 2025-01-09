2c5253cf8770394cf64e2af16271b883101b8c1c
Damien <damients@gmail.com>
2025-01-09 15:56:29 +0100

    temp

---
+++ app/wwwroot/gameGrid.js
@@ -0,0 +1,18 @@
+﻿const createGrid = () => {
+    const gridContainer = document.getElementById('grid');
+
+    for (let i = 0; i < 11 * 11; i++) {
+        const cell = document.createElement('input');
+        cell.type = 'text';
+        cell.maxLength = 1;
+        cell.className = 'grid-item'
+        cell.addEventListener('input', (e) => {
+
+            e.target.value = e.target.value.toUpperCase();
+        });
+        gridContainer.appendChild(cell);
+    }
+};
+
+const startTimer = (duration) => {}
+

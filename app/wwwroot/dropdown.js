760c5742945c294ea28e007f071cd855b8937f4a
Sebbe <Sebbelarsson9601@gmail.com>
2025-01-09 14:27:40 +0100

    Added Dropdown.js and DropDownMenu.html

---
+++ app/wwwroot/dropdown.js
@@ -0,0 +1,14 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    const dropdownContainer = document.querySelector(".dropdown-container")
+    const dropdownButton = document.querySelector(".dropdown-button")
+
+    dropdownButton.addEventListener("click", () => {
+        dropdownContainer.classList.toggle("show")
+    })
+    
+    document.addEventListener("click", (event) => {
+        if (!dropdownContainer.contains(event.target)) {
+            dropdownContainer.classList.remove("show")
+        }
+    })
+})

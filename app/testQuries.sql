dc8c6a61782e852080e4dd6250ab8625564808ca
Editor Jakupi <da.editoor@msn.com>
2025-01-09 14:18:04 +0100

    Added an endgame button

---
+++ app/testQuries.sql
@@ -0,0 +1,14 @@
+﻿
+--infoga ett spel i "lobby" status
+INSERT INTO games (game_id, player_1, player_2, state, gamecode)
+VALUES (1, 'PlayerOne', NULL, 'lobby', 'ABC123');
+
+
+--infoga ett aktivt spel med två spelare
+INSERT INTO games (game_id, player_1, player_2, state, gamecode)
+VALUES (2, 'PlayerOne', 'PlayerTwo', 'active', 'DEF456');
+
+
+--infoga ett avslutat spel
+INSERT INTO games (game_id, player_1, player_2, state, gamecode)
+VALUES (3, 'PlayerOne', 'PlayerTwo', 'ended', 'GHI789');

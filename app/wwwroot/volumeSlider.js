fbefc248e2063e5edea67e19dfa52cd442f74b82
Sebbe <Sebbelarsson9601@gmail.com>
2025-01-09 13:56:30 +0100

    Added a volume Slider to Index.html that is changing the volume on the side using a eventListener and DOMContentLoaded

---
+++ app/wwwroot/volumeSlider.js
@@ -0,0 +1,14 @@
+﻿document.addEventListener("DOMContentLoaded", () => {
+    const volumeSilder = document.getElementById("volume-sliderl");
+    const audioElements = document.querySelectorAll("audio")
+    
+    audioElements.forEach(media => {
+        media.volume = volumeSilder.value;
+    })
+    
+    volumeSilder.addEventListener("input", () => {
+        audioElements.forEach(media => {
+            media.volume = volumeSilder.value;
+        })
+    })
+})

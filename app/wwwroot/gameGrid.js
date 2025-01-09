b78965003861e602979af78c9875a8f599124bf9
Damien <damients@gmail.com>
2025-01-09 16:24:45 +0100

    temp

--- app/wwwroot/gameGrid.js
+++ app/wwwroot/gameGrid.js
@@ -14,5 +14,34 @@
     }
 };
 
-const startTimer = (duration) => {}
+const startTimer = (duration) => {
+    const timerElement = document.getElementById('startTimer');
+    let remainingTime = duration;
+
+    const updateTimerDisplay = () => {
+        const minutes = Math.floor(remainingTime / 60);
+        const seconds = remainingTime % 60;
+        timerElement.textContent = `Time Remaining: ${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;;
+    };
+
+    const countdown = setInterval(() => {
+        if (remainingTime <=0) {
+            clearInterval(countdown);
+            timerElement.textContent = "Time's Up!";
+            alert("Game Over!");
+        }
+    }, 1000);
+
+    updateTimerDisplay();
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+    createGrid();
+    startTimer(180);
+})
+
+
+
+
+
 

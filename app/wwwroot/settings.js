867d2ec730038760c8b0c7e03210d91a74250150
Sebbe <Sebbelarsson9601@gmail.com>
2025-01-10 11:58:46 +0100

    Uppdaterade alla Settings så dem har debugs i consolen där man lägger in kod sen la till knapp som går till index.html

---
+++ app/wwwroot/settings.js
@@ -0,0 +1,23 @@
+﻿
+const roundTimeInput = document.getElementById('round-time');
+const numberOfRoundsInput = document.getElementById('number-of-rounds');
+const extraPointsCheckbox = document.getElementById('extra-points');
+
+
+//Just nu bara ger den logs i consolen för inputsen man gör i menyn
+//**TODO
+// *Lägga till funktionerna för knapparna så dem modifererars
+roundTimeInput.addEventListener('input', () => {
+    console.log('Rundtid:', roundTimeInput.value);
+    
+});
+
+numberOfRoundsInput.addEventListener('input', () => {
+    console.log('Antal rundor:', numberOfRoundsInput.value);
+    
+});
+
+extraPointsCheckbox.addEventListener('change', () => {
+    console.log('Extra poängs rutor:', extraPointsCheckbox.checked);
+    
+});

111066b10cc4f1b07695c41a3ed98a12440b2f12
Editor Jakupi <da.editoor@msn.com>
2025-01-15 00:24:17 +0100

    Added js function to remove a letter from the grid on double click

--- app/wwwroot/LetterUI/letterUI.js
+++ app/wwwroot/LetterUI/letterUI.js
@@ -60,3 +60,44 @@ tile.mouseup(function() {
 // }
 //
 // $('.tile').mousedown(handle_mousedown)
+
+
+// Add event listener for double-click to remove a placed tile
+$(document).ready(function() {
+    $('.grid-item').on('dblclick', function() {
+        // Check if there's a placed tile
+        if ($(this).children('.placed-tile').length > 0) {
+            $(this).children('.placed-tile').remove(); // Remove the placed tile
+        }
+    });
+});
+
+// Existerande kod för draggable
+var tile = $(".letterUI > * > .tile")
+
+tile.hover(function(){
+    $(this).animate({"background-color": "#ffdead"}, {queue: false, duration: 200});
+}, function(){
+    $(this).animate({"background-color": "#faebd7"}, {queue: false, duration: 200});
+});
+
+$(function() {
+    tile.draggable();
+
+    $(".grid-item").droppable({
+        drop: function(event, ui) {
+            if($(this).children().length < 1) {
+                const clone = $(ui.draggable).clone().appendTo($(this));
+                clone.css({"top":"", "left":"", "background-color": "#faebd7", "border": "1px solid"});
+                clone.addClass("placed-tile");
+            } else {
+                $(ui.draggable).animate({"background-color": "#cd5c5c"}, {queue: true, duration: 10});
+                $(ui.draggable).animate({"background-color": "#faebd7"}, {queue: true, duration: 500});
+            }
+        }
+    });
+});
+
+tile.mouseup(function() {
+    tile.animate({"top":"", "left":""}, {queue: false});
+});
